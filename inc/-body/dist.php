2b5d034e3a751ed12f4ec520278c5084332212f2
Vincent Valentin <vincent@htmlzengarden.com>
2013-03-28 17:48:29 +0100

    fonctionnel sous IE maintenant et avec deux démos desktop/mobile first

--- inc/-body/dist.php
+++ inc/-body/dist.php
@@ -1,11 +1,12 @@
 <!-- include -body -->
-<!--[if IE 9]>  <body class="ie ie9"><![endif]-->
-<!--[if IE 8]>  <body class="ie ie8"><![endif]-->
-<!--[if IE 7]>  <body class="ie ie7"><![endif]-->
-<!--[if IE 6]>  <body class="ie ie6"><![endif]-->
-<!--[if !IE]>--><body>           <!--<![endif]-->
+<!--[if IE 9]>  <body class="ie ie9"    ><![endif]-->
+<!--[if IE 8]>  <body class="ie ie8"    ><![endif]-->
+<!--[if IE 7]>  <body class="ie ie7"    ><![endif]-->
+<!--[if IE 6]>  <body class="ie ie6"    ><![endif]-->
+<!--[if !IE]>--><body class="not-ie"><!--<![endif]-->
 	<script src="../js/lib/browserdetect.js"  ></script>
 	<script src="../js/init.js"               ></script>
+	<script src="../js/lib/modernizr.js"      ></script>
 <?php include Z::getFile('page'); ?>		
 	<script src="../js/lib/jquery.js"         ></script>
 	<script src="../js/lib/jquery.baseline.js"></script>
--- inc/page/dist.php
+++ inc/page/dist.php
@@ -1,5 +1,5 @@
 <!-- include page -->
-<div id="page">
+<div id="page" class="<?php echo Z::getPageName();?>">
 <?php include Z::getFile('header' ); ?>
 <?php include Z::getFile('content'); ?>
 <?php include Z::getFile('footer' ); ?>
