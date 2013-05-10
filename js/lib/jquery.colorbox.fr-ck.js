db08a65aae1920fb07117beb5f131b73d5854f1e
Vincent Valentin <vincent@htmlzengarden.com>
2013-05-10 10:44:02 +0200

    ménage + bourbon étendu

--- js/lib/jquery.colorbox.fr-ck.js
+++
@@ -1,5 +0,0 @@
-/*
-  jQuery Colorbox language configuration
-  language: French (fr)
-  translated by: oaubert
-*/jQuery.extend(jQuery.colorbox.settings,{current:"<span>{current}/{total}</span>",previous:"<span>Précédente</span>",next:"<span>Suivante</span>",close:"<span>Fermer</span>",xhrError:"<span>Impossible de charger ce contenu.</span>",imgError:"<span>Impossible de charger cette image.</span>",slideshowStart:"<span>Lecture</span>",slideshowStop:"<span>Pause</span>"});
