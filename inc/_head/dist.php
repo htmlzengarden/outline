37fbc253ab9087450fad2cf28d25d7193c78498f
Marie Guillaumet <mhguillaumet@clever-age.com>
2013-12-13 17:21:35 +0100

    Mise à jour de la balise meta viewport selon les recommandations de Raphaël Goetter (cf. http://blog.goetter.fr/post/32513655620/viewport-adieu-width-device-width)

--- inc/_head/dist.php
+++ inc/_head/dist.php
@@ -2,7 +2,7 @@
 	<meta charset="UTF-8"															   />
 
 	<!--<meta name="viewport" content="width=1024" /> -->
-	<meta name="viewport" content="initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!--[if lte IE 8]>
     <link rel="stylesheet"       type="text/css"     href="../assets/css/styles-lte-ie8.css"  />
